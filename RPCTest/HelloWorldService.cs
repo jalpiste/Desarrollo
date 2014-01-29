532ff9f97a2ad9c4b7f7f05af214b867ae80b54f
jalpiste <jlalpiste@gmail.com>
2014-01-29 15:41:30 -0500

    Proyecto Total

---
+++ RPCTest/HelloWorldService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AustinHarris.JsonRpc;
+namespace RPCTest
+{
+    public class HelloWorldService : JsonRpcService
+    {
+        [JsonRpcMethod]
+        private string helloWorld(string message)
+        {
+            return "Hello World " + message;
+        }
+
+    }
+}
